c9099387ad98722bbefb6c55f7ba4635c2ab82bc
eastpiger <lvwt@shanghaitech.edu.cn>
2015-05-21 15:44:29 +0800

    a

--- jni/src/core/ImageProc.cpp
+++ jni/src/core/ImageProc.cpp
@@ -42,7 +42,41 @@ JNIEXPORT jbyteArray JNICALL Java_com_example_carplate_CarPlateDetection_ImagePr
 	char* img = jstring2str(env,imgpath);
 	char* svm = jstring2str(env,svmpath);
 	char* ann = jstring2str(env,annpath);
-	Mat src = imread(img);
+
+	// #############################################################
+	// Mat src = imread(img);
+	Mat src;
+	src.create(480,640, CV_8UC3);
+
+	FILE *in;
+	in=fopen(img,"r");
+
+	int i,j,y=0,u=0,v=0;
+	char t,handle=1;
+	double R,G,B;
+	for (j=0;j<480;j++){
+		for (i=0;i<640;i++){
+			fscanf(in,"%X",&y);
+			if (handle){
+				fscanf(in,"%X",&u);
+				handle=0;
+			}else{
+				fscanf(in,"%X",&v);
+				handle=1;
+			}
+
+			R=(y+1.4075*(v-128));
+			G=(y-0.455*(u-128)-0.7169*(v-128));
+			B=(y+1.779*(u-128));
+
+			src.at<Vec3b>(j,i)[0] = (unsigned char)(B<0)?0:(B>255)?255:(unsigned char)B;
+			src.at<Vec3b>(j,i)[1] = (unsigned char)(G<0)?0:(G>255)?255:(unsigned char)G;
+			src.at<Vec3b>(j,i)[2] = (unsigned char)(R<0)?0:(R>255)?255:(unsigned char)R;
+		}
+	}
+	fclose(in);
+
+	// #############################################################
 	pr.LoadSVM(svm);
 	pr.LoadANN(ann);
 
@@ -61,7 +95,8 @@ JNIEXPORT jbyteArray JNICALL Java_com_example_carplate_CarPlateDetection_ImagePr
 	string str = "0";
 
 	if (count == 0) {
-		str = plateVec[0];
+		str="NONE";
+		//str = plateVec[0];
 	}
 
 	char *result = new char[str.length() + 1];
